50f61e25f946fc15c21ea0611cde53c5d6ecff74
Byshov <byshov1998@gmail.com>
2026-03-02 14:25:39 +0100

    done type of objects

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+//Primitives
+
 let a: string = "word";
 let b: number = 5;
 let c: boolean = true;
@@ -19,3 +21,75 @@ g = 5;
 if (typeof g === "number") {
   g.toFixed(2);
 }
+
+// Objects
+
+interface Car {
+  readonly VIN: string;
+  engine: string;
+  color: string;
+  weight?: number;
+  number: string;
+}
+
+const car:Car = {
+  VIN:"EH3432h342jJ32kk",
+  engine: "v8",
+  color: "red",
+  weight: 2000,
+  number: "AX8472AA",
+}
+
+
+const carImport:Car = {
+  VIN:"EH3432h3das21jJd22kk",
+  engine: "v12",
+  color: "green",
+  number: "PL3676XC",
+}
+
+car.color = "blue";
+
+// car.VIN = "asjduiqdqwdqw"; // error , because readonly
+interface User {
+  name: string;
+  email: string;
+}
+let user:null | User = null;
+
+user = {
+  name: "Vlad",
+  email:"vlados@gmail.com",
+}
+
+type Status = "fulfilled" | "rejected" | "pending";
+
+let status:Status = "fulfilled";
+
+status = "rejected";
+
+// interface Animal {
+//   name: string;
+//   breed: string;
+// }
+//
+// interface Animal {
+//   color: string;
+// }
+
+type Animal = {
+  name: string;
+  breed: string;
+}
+
+type SuperAnimal = Animal & {
+  color: string;
+};
+
+const dog: SuperAnimal = {
+  name: "Sobaka",
+  breed: "Ovcharka",
+  color: "brown",
+}
+
+

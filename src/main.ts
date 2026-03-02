60d869bed3838780c2a8a1a29dd8ec5b95c4e0ba
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-03-02 15:28:46 +0100

    arrays, functions

--- src/main.ts
+++ src/main.ts
@@ -32,21 +32,20 @@ interface Car {
   number: string;
 }
 
-const car:Car = {
-  VIN:"EH3432h342jJ32kk",
+const car: Car = {
+  VIN: "EH3432h342jJ32kk",
   engine: "v8",
   color: "red",
   weight: 2000,
   number: "AX8472AA",
-}
+};
 
-
-const carImport:Car = {
-  VIN:"EH3432h3das21jJd22kk",
+const carImport: Car = {
+  VIN: "EH3432h3das21jJd22kk",
   engine: "v12",
   color: "green",
   number: "PL3676XC",
-}
+};
 
 car.color = "blue";
 
@@ -55,16 +54,19 @@ interface User {
   name: string;
   email: string;
 }
-let user:null | User = null;
+
+// Union types
+
+let user: null | User = null;
 
 user = {
   name: "Vlad",
-  email:"vlados@gmail.com",
-}
+  email: "vlados@gmail.com",
+};
 
 type Status = "fulfilled" | "rejected" | "pending";
 
-let status:Status = "fulfilled";
+let status: Status = "fulfilled";
 
 status = "rejected";
 
@@ -80,7 +82,7 @@ status = "rejected";
 type Animal = {
   name: string;
   breed: string;
-}
+};
 
 type SuperAnimal = Animal & {
   color: string;
@@ -90,6 +92,96 @@ const dog: SuperAnimal = {
   name: "Sobaka",
   breed: "Ovcharka",
   color: "brown",
+};
+
+// Arrays
+
+const numbers: number[] = [1, 2, 3];
+
+type Values = boolean | string | number;
+
+const arr: Values[] = [true, "hello", 43, false];
+
+const arr1: (string | number)[] = [56, "word", 46];
+
+interface User1 {
+  name: string;
+  age: number;
 }
 
+const arr2: User1[] = [
+  { name: "Ivan", age: 21 },
+  { name: "Petro", age: 26 },
+];
 
+const arr3: (User1 | SuperAnimal)[] = [{ name: "Ivan", age: 21 }, dog];
+
+// * Functions
+
+// 1. Просте додавання чисел
+
+// Умова:
+// Напиши функцію add, яка приймає два числа та повертає їх суму.
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+add(4, 8);
+
+// 2. Привітання користувача
+
+// Умова:
+// Функція greetUser приймає ім’я (string) і логічне значення isMorning (boolean).
+// Повертає рядок "Good morning, {name}" якщо isMorning === true, і "Hello, {name}" якщо false.
+
+function greetUser(name: string, isMorning: boolean): string {
+  return isMorning ? `Good morning, ${name}` : `Hello, ${name}`;
+}
+
+greetUser("Yuliia", true);
+
+// 3. Перевірка числа
+
+// Умова:
+// Функція isPositive приймає будь-яке значення (unknown) і повертає boolean:
+
+// true якщо це число > 0,
+
+// false якщо число ≤ 0 або не число.
+
+function isPositive(value: unknown): boolean {
+  if (typeof value !== "number") {
+    return false;
+  }
+  return value > 0;
+}
+
+isPositive(5);
+
+// 4. Об’єкт користувача
+
+// Умова:
+// Створи інтерфейс User з полями:
+
+// name: string
+
+// age: number
+
+// email?: string (optional)
+
+// Функція getUserSummary приймає об’єкт User та повертає рядок "Name: {name}, Age: {age}, Email: {email}".
+// Якщо email немає — виводимо "Email: N/A".
+
+interface User3 {
+  name: string;
+  age: number;
+  email?: string;
+}
+
+function getUserSummary(user: User3): string {
+  const email = user.email ?? "N/A";
+  return `Name: ${user.name}, Age: ${user.age}, Email: ${email}`;
+}
+
+getUserSummary({ name: "Ivan", age: 34 });

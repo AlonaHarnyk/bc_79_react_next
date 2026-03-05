b1d32a7f1874714dc8b1d698ead2ebceefc9d6da
vjosik <108922728+vjosik@users.noreply.github.com>
2026-03-05 14:40:45 +0200

    task-1

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from "react-dom/client";
+import { students } from "./data";
+
+// const student = students[0];
+
+// const markup = (
+//   <>
+//     <p>{student.name}</p> <p>{student.age} </p>
+//     {/* <p>{String(student.isOnline)}</p> */}
+//     {/* <p>Status online:{student.isOnline ? "yes" : "no"}</p> */}
+//     {/* {student.isOnline ? <p>Status online: yes</p> : <h1>Status online: no</h1>} */}
+//     {student.isOnline && <p>Status online: yes</p>}
+//   </>
+// );
+
+const markup = (
+  <ul>
+    {students.map((student) => (
+      <li key={student.id}>
+        <h2>{student.name}</h2>
+      </li>
+    ))}
+  </ul>
+);
+
+createRoot(document.getElementById("root")!).render(markup);

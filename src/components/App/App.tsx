4fb83d20cc41b5f691dece283ce7909ccf653e4c
Anastasia-Zahliada <zahliadaa@gmail.com>
2026-03-10 16:29:46 +0100

    add tabs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,14 +7,27 @@
 
 // Cтворити форму додавання користувача (форма має містити 2 текстових інпути - для введення імені та пошти). Форма має відкриватись по кнопці Add user, яка має рендеритись під списком юзерів (за уиови, що останній відрендерений). Коли форма відкрита, кнопка Add user має зникнути. При сабміті форми дані мають виводитись в консоль, форма зникати, натомість повертатись кнопка Add user.
 
+import { useState } from "react";
 import { Books } from "../Books/Books.tsx";
 import { Users } from "../Users/Users.tsx";
 
+type Tab = "users" | "books"
+
 function App() {
+  const [activeTab, setActiveTab] = useState<Tab>("users")
   return (
     <>
-      <Users />
-      <Books />
+      <ul>
+        <li>
+          <button onClick={()=>{setActiveTab("users")}}>Show users</button>
+        </li>
+        <li>
+          <button onClick={()=>{setActiveTab("books")}}>Show books</button>
+        </li>
+      </ul>
+      {activeTab === "users" && <Users />} 
+      {activeTab === "books" && <Books />}
+      
     </>
   );
 }
